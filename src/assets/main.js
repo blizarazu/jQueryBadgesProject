26d1b0d559a8b2a00817c784e2ef2cfa16115d9b
Beñat Lizarazu <blizarazu@gmail.com>
2017-07-02 21:44:19 +0200

    Show Code School badges

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,21 @@
 $(function() {
-
-  // your code will go here
-
+  $.ajax({
+    url: 'https://www.codeschool.com/users/blizarazu.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      var completed = response.courses.completed;
+      for(var i=0; i < completed.length; i++) {
+        $('#badges')
+        .append(
+          '<div class="course">' +
+            '<h3>' + completed[i].title + '</h3>' +
+            '<img src="' + completed[i].badge + '"></img>' +
+            '<a href="' + completed[i].url + '" target="_blank" class="btn btn-primary">' +
+              'See Course' +
+            '</a>' +
+          '</div>'
+        );
+      }
+    }
+  });
 });
